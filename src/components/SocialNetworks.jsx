f17264f1929dd87302733beef25baa5f71978173
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-08-27 17:52:48 -0300

    adicionando link e pronto pra começar o responsivo

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,16 +3,16 @@ import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa'
 
 
 const socialNetworks =[
-    { name: "linkedin", icon: <FaLinkedinIn />},
-    { name: "github", icon: <FaGithub />},
-    { name: "instagram", icon: <FaInstagram />},
+    { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/gabrielsantospessanha/"},
+    { name: "github", icon: <FaGithub />, link: "https://github.com/gabrielspessanha"},
+    { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/bieldinn/" },
 ]
 
 export function SocialNetworks(){
     return(
        <section id="social-networks"> 
         {socialNetworks.map((network)=> (
-            <a href="" className="social-btn" id={network.name} key={network.name}>
+            <a href={network.link} target="__blank" className="social-btn" id={network.name} key={network.name}>
                 {network.icon}
             </a>
         ))}

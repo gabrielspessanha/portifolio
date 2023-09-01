f133e1a6114536402618d61ae6de3888ff0894b4
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-09-01 16:09:51 -0300

    fazendo navbar e sidebar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,29 +11,30 @@ export function Header(){
     return( 
       <>
         <Navbar bg="transparent"  expand="lg" className={`header container`}>
-          <Container fluid>
+          <Container>
           <Navbar.Brand href="#home">
-            <p>Gabriel Pessanha</p>
+            <h3>Gabriel Pessanha</h3>
           </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-lg`}
               aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
               placement="end"
+              className='sidebar'
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
                   Gabriel Santos pessanha
                 </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Sobre</Nav.Link>
-                  <Nav.Link href="#action1">Portfolio</Nav.Link>
-                  <Nav.Link href="#action1">Contato</Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 navLinks">
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="#action2">Sobre</Nav.Link>
+                    <Nav.Link href="#action1">Portfolio</Nav.Link>
+                    <Nav.Link href="#action1">Contato</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
         </Navbar>

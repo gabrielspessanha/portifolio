ef4b66206b5d84cf7d94031c1075192d6832aa07
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-09-01 09:42:41 -0300

    conectando ao repositorio do githuv

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -7,7 +7,7 @@ export function ProjectsContainer(){
        <section className="projects-container">
             <h2>Projetos</h2>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, odio eaque accusantium facere totam libero, ex facilis ipsa fuga, veniam voluptates deserunt adipisci illum architecto quos ea quo mollitia blanditiis.</p>
+            <p>Através da minha jornada de aprendizado e aprimoramento em programação, tive a oportunidade de trabalhar em projetos que não apenas solidificaram meu conhecimento, mas também demonstraram minha capacidade de aplicar conceitos em cenários do mundo real. Abaixo, você encontrará uma seleção de alguns dos projetos que tive o prazer de criar até agora.</p>
 
             <a href="" className="btn">
                 ver projetos

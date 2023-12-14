abdb5544b63a5111f18f50ab04dee55850f1163c
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-12-14 13:36:11 -0300

    atualizando a seção de projetos e melhorando sua responsividade

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -1,29 +1,17 @@
-import{
-    DiHtml5,
-    DiCss3,
-    DiJsBadge,
-    DiReact
-} from 'react-icons/di' ;
+import { Tech } from '@/services/information';
 
 import './styles.scss';
-
-
-const technologies = [
-    { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-    { id: "css", name: "CSS3", icon: <DiCss3 />},
-    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-    { id: "react", name: "React", icon: <DiReact />}
-  ];
 
 export function Technologies(){
     return(
         <section className='technologies-container '>
             <h2>TECNOLOGIAS</h2>
             <hr />
-            <div className="flex flex-wrap justify-center gap-4">
-                {technologies.map((tech) => (
-                    <div className="technology-card w-72" id={tech.id} key={tech.id}>
-                        <h3> {tech.icon} {tech.name} </h3> 
+            <div className="flex flex-wrap justify-center gap-4 technologiesContent">
+                {Tech.map((tech) => (
+                    <div className="technology-card" id={tech.id} key={tech.id}>
+                        {tech.icon} 
+                        <p className='flex items-center'>{tech.name}</p> 
                     </div>
                 ))}
             </div>
--- src/services/information.tsx
+++ src/services/information.tsx
@@ -2,8 +2,13 @@ import{
   DiHtml5,
   DiCss3,
   DiJsBadge,
-  DiReact
+  DiReact,
+  DiNodejs
 } from 'react-icons/di' ;
+
+import { FaBootstrap, FaSass, FaGithub, FaGitAlt } from "react-icons/fa";
+import { TbBrandNextjs, TbFileTypeSql} from "react-icons/tb";
+import { SiTypescript, SiTailwindcss} from "react-icons/si";
 
 const tech = {
   react: <DiReact color='#61dafb' size={50} key={1} />,
@@ -11,6 +16,24 @@ const tech = {
   css: <DiCss3 color='#1572b6' size={50}  key={3} />,
   html: <DiHtml5 color='#E44D26' size={50}  key={4} />
 }
+
+export const Tech = [
+  { id: "js", name: "JavaScript", icon: <DiJsBadge color='#f0db4f' size={50} /> },
+  { id: "react", name: "React", icon: <DiReact color='#61dafb' size={50} />},
+  { id: "typescript", name: "Typescript", icon: <SiTypescript color='#3178c6' size={50}/>},
+  { id: "nextjs", name: "Nextjs", icon: <TbBrandNextjs  size={50}/>},
+  { id: "sql", name: "Sql", icon: <TbFileTypeSql color='#ed740a' size={50}/>},
+  { id: "html", name: "HTML5", icon: <DiHtml5 color='#E44D26' size={50} /> },
+  { id: "css", name: "CSS3", icon: <DiCss3 color='#1572b6' size={50} />},
+  { id: "node", name: "Node.js", icon: <DiNodejs color='rgb(136, 194, 97)' size={50}/>},
+  { id: "git", name: "Git", icon: <FaGitAlt color='rgb(240, 85, 57)' size={50}/>},
+  { id: "github", name: "Github", icon: <FaGithub color='#2c3160' size={50}/>},
+  { id: "tailwind", name: "Tailwind", icon: <SiTailwindcss color='rgb(32, 186, 189)' size={50}/>},
+  { id: "sass", name: "Sass", icon: <FaSass color='rgb(237, 70, 126)' size={50}/>},
+  { id: "bootstrap", name: "Bootstrap", icon: <FaBootstrap color='rgb(137, 19, 252)' size={50}/>},
+];
+
+
 
 export const Infocards = [
   {   
@@ -50,3 +73,4 @@ export const Infocards = [
       urlImg: "/img/falcon.png"
   }
 ]
+
